497c6b37169917cec28181f0e36c0a29a586b053
Jure Sternad <sternad.jure@gmail.com>
2020-05-12 18:22:40 +0200

    Dodan model

---
+++ model.py
@@ -0,0 +1,62 @@
+import random
+
+STEVILO_DOVOLJENIH_NAPAK = 10
+PRAVILNA_CRKA = '+'
+PONOVLJENA_CRKA = 'o'
+NAPACNA_CRKA = '-'
+ZMAGA = 'W'
+PORAZ = 'X'
+
+class Igra:
+    def __init__(self, geslo, crke=None):
+        self.geslo = geslo
+        self.crke = [] if crke is None else [crka.upper() for crka in crke]
+
+    def napacne_crke(self):
+        return [crka for crka in self.crke if crka not in self.geslo]
+
+    def pravilne_crke(self,crke):
+        return [crka for crka in self.crke if crka in self.geslo]
+
+    def stevilo_napak(self):
+        return len(self.napacne_crke())
+
+    def zmaga(self):
+        return set(self.geslo) == set(self.crke)
+        # ali return all(crka in self.crke for crka in self.geslo)
+    def poraz(self):
+        return self.stevilo_napak() >= STEVILO_DOVOLJENIH_NAPAK
+
+    def pravilni_del_gesla(self):
+        s = ''
+        for crka in self.geslo:
+            s += crka if crka in self.crke else '_'
+        return s
+
+    def nepravilni_ugibi(self):
+        return ' '.join(self.napacne_crke())
+        
+    def ugibaj(self, ugibana_crka):
+        ugibana_crka = ugibana_crka.upper()
+        if ugibana_crka in self.crke:
+            return PONOVLJENA_CRKA
+        else:
+            if ugibana_crka in self.geslo:
+                if self.zmaga():
+                    return ZMAGA
+                else:
+                    return PRAVILNA_CRKA
+            else:
+                if self.poraz():
+                    return PORAZ
+                else:
+                    return NAPACNA_CRKA
+
+    
+bazen_besed = []
+for beseda in open('besede.txt', encoding='utf-8'):
+    bazen_besed.append(beseda.strip())
+
+def nova_igra():
+    beseda = random.choice(bazen_besed)
+    return Igra(beseda)
